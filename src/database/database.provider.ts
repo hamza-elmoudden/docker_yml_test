360f64c3b70ad953f5278c454b354a4fbbdf1464
Hamza Elmouddane <hamzaelmouddane2000@gmail.com>
2026-05-01 20:14:03 +0100

    update import env

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,15 +1,17 @@
 // database.provider.ts
+import { ConfigService } from '@nestjs/config';
 import { Pool } from 'pg';
 
 export const databaseProvider = {
   provide: 'PG_POOL',
-  useFactory: async () => {
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService) => {
     const pool = new Pool({
-      host: 'postgres',
-      port: 5432,
-      user: 'postgres',
-      password: '123456789',
-      database: 'mydb',
+      host: config.get('DB_HOST'),
+      port: config.get<number>('DB_PORT'),
+      user: config.get('DB_USER'),
+      password: config.get('DB_PASS'),
+      database: config.get('DB_NAME'),
     });
 
     return pool;
